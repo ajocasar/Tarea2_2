c3bc663e897c3e08d5f9cb75fee72767eabc62f3
ajocasar <99230237+ajocasar@users.noreply.github.com>
2022-11-14 18:42:41 -0600

    Add project files.

---
+++ Tarea2_2/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using SignaturePad.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Tarea2_2.Models;
+
+
+
+namespace Tarea2_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            
+            var path = System.IO.Path.Combine("", "FirmaTest.jpg");
+
+            Stream image_= await   padView.GetImageStreamAsync(SignatureImageFormat.Jpeg);
+            BinaryReader br = new BinaryReader(image_);
+            br.BaseStream.Position = 0;
+            Byte[] All = br.ReadBytes((int)image_.Length);
+
+
+            if ( String.IsNullOrWhiteSpace(txtDescripcion.Text) || String.IsNullOrWhiteSpace(txtNombre.Text))
+            {
+                await DisplayAlert("Error Introducciendo los datos", "¡Llenar todos los campos!", "OK");
+            }
+            else
+            {
+                var emple = new constructor
+                {
+                    nombre = txtNombre.Text,
+                    descripcion = txtDescripcion.Text,
+                    imgM = (byte[])All
+                
+            };
+                var resultado = await App.BaseDatos.EmpleadoGuardar(emple);
+                if (resultado != 0)
+                {
+                    await DisplayAlert("Alerta", "¡Firma Guardada con éxito!", "OK");
+                    txtNombre.Text = "";
+                    txtDescripcion.Text = "";
+                    padView.Clear();
+
+                }
+                else
+                {
+                    await DisplayAlert("Alerta", "¡Los datos no se guardaron!", "OK");
+                }
+            }
+                                                              
+        }
+      
+        private async void btnLista_Clicked(object sender, EventArgs e)
+        {
+            var newpage = new lista();
+            await Navigation.PushAsync(newpage);
+        }
+    }
+}
---
+++ Tarea2_2/Models/basededatos.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using Tarea2_2.Models;
+using System.Threading.Tasks;
+
+namespace Tarea2_2.Models
+{
+    public class basededatos
+    {
+        readonly SQLiteAsyncConnection db;
+        public basededatos()
+        {
+        }
+        // contructor
+        public basededatos(String pathbasedatos)
+        {
+            db = new SQLiteAsyncConnection(pathbasedatos);
+
+            // Crear tablas en la BD
+            db.CreateTableAsync<constructor>();
+
+        }
+
+       //EnLista la tabla empleados
+        public Task<List<constructor>> listaempleados()
+        {
+
+            return db.Table<constructor>().ToListAsync();
+        }
+
+        // buscar empleados
+        public Task<constructor> ObtenerEmpleado(Int32 pcodigo)
+        {
+            return db.Table<constructor>().Where(i => i.codigo == pcodigo).FirstOrDefaultAsync();
+        }
+
+        // Para guardar o actualizar los datos de un empleado
+
+        public Task<Int32> EmpleadoGuardar(constructor emple)
+        {
+            if (emple.codigo != 0)
+            {
+                return db.UpdateAsync(emple);
+            }
+            else
+            {
+                return db.InsertAsync(emple);
+            }
+        }
+
+        //Eliminar
+
+        public Task<Int32> EmpleadoBorrar(constructor emple)
+        {
+            return db.DeleteAsync(emple);
+        }
+    }
+}
